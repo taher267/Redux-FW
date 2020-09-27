395cd9b3a4f914a1543090892484dfc90b4d9a9a
ABU TAHER <65433832+taher267@users.noreply.github.com>
2020-09-28 05:05:56 +0600

    Create sample-config.php

---
+++ sample-config.php
@@ -0,0 +1,8 @@
+'admin_bar'            => false,
+ 'dev_mode'             => false,
+ 'update_notice'        => false,
+ 'customizer'           => false,
+ 'save_defaults'        => true,
+ 'show_import_export'   => false,
+ 'output'               => false,
+'output_tag'           => false,
